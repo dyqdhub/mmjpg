9fc9d7e3a31cc7f73d2f3a57e2498932afa9b198
unknown <duyanqi501@163.com>
2018-10-26 17:41:53 +0800

    1,0,OK

--- mmjpg.com.py
+++ mmjpg.com.py
@@ -2,35 +2,69 @@
 # -*- coding:utf-8 -*-
 
 import os
-import urllib.request
+import requests
 from lxml import etree
-
 from pyquery import PyQuery
+
 
 url = "http://www.mmjpg.com/"
 
-# 获取首页内容所有图片详情链接
-response = urllib.request.urlopen(url, timeout=3)
-html = response.read().decode('utf-8')
-html_xml = etree.HTML(html)
-imgurl_list = html_xml.xpath('//div[@class="pic"]/ul/li/a/@href')
 
-#获取详情页图片
-re = urllib.request.urlopen(url=imgurl_list[0])
-img_html = re.read().decode('utf-8')
-
-#获取详情页图片信息
-img_html_xml = etree.HTML(img_html)
-img_page = img_html_xml.xpath('//div[@id="content"]/a/@href')[0]
-img_url = img_html_xml.xpath('//div[@id="content"]/a/img/@src')[0]
-img_name = img_html_xml.xpath('//div[@id="content"]/a/img/@alt')[0]
+#下载图片并保存
+def download_save(img_page_url,img_url,name):
+    header = {
+        'Referer': img_page_url,
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.81 Safari/537.36',
+    }
+    file_name = img_url[-8:]
+    img_rs = requests.get(img_url, headers=header)
+    # print(img_rs.status_code)
+    img_by = img_rs.content
+    file = open('{}\{}'.format(name,file_name), 'wb')
+    file.write(img_by)
 
 
+#爬取首页所有图片的地址
+def get_index_url(page):
+    index_url_list = []
+    for num in range(1, page+1):
+        print("正在爬取第%s页" % str(num))
+        if num == 1:
+            index_page = url
+        else:
+            index_page = url + 'home/%s' % num
+        re_index_html = requests.get(index_page)
+        re_index_html.encoding="utf-8"
+        index_html = re_index_html.text
+        index_html_xml = etree.HTML(index_html)
+        index_img_url_list = index_html_xml.xpath('//div[@class="pic"]/ul/li/a/@href')
+        for l in index_img_url_list:
+            index_url_list.append(l)
+    return index_url_list
 
-def save_img(img_url):
-    re_img = urllib.request.urlopen(img_url)
-    re_img_by = re_img.read()
-    file = open(img_url[-8:], 'wb')
-    file.write(re_img_by)
+#获取图集页面的图片地址
+def get_img_url(url_list):
+    for url in url_list:
+        re_img_html = requests.get(url)
+        re_img_html.encoding="utf-8"
+        img_html = re_img_html.text
+        img_html_xml = etree.HTML(img_html)
+        #获取图集的名称和页数，并循环页数
+        img_html_name = img_html_xml.xpath('//div[@class="article"]/h2/text()')[0]
+        print("正在爬取：%s" % img_html_name)
+        os.mkdir(img_html_name)
+        page_nu = img_html_xml.xpath('//div[@class="page"]/a[last()-1]/text()')[0]
+        for nu in range(1, int(page_nu)+1):
+            #拼接获得每页图片的URL
+            img_page_url = url + '/' + str(nu)
+            r_i_h = requests.get(img_page_url)
+            r_i_h.encoding="utf-8"
+            i_h = r_i_h.text
+            i_h_x = etree.HTML(i_h)
+            img_url = i_h_x.xpath('//div[@id="content"]/a/img/@src')[0]
+            download_save(img_page_url,img_url,img_html_name)
 
 
+if __name__ == '__main__':
+    url_list = get_index_url(1)
+    get_img_url(url_list)
